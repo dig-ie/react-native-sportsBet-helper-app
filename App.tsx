3eca3247e746a743322cf9462b91a2d00b6dc1ba
jhonata <jcvdbarros@gmail.com>
2025-04-15 11:34:04 -0300

    feat: integrate react-query for data fetching and state management

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import AppRoutes from "@/routes";
 import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 export default function App() {
-  return <AppRoutes />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppRoutes />
+    </QueryClientProvider>
+  );
 }
